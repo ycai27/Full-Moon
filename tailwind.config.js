b1f2e471b30a919aac6af62c91b304df00421424
ycai27 <yannancai27@gmail.com>
2024-07-24 15:37:25 -0700

    index accordion general layout created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",
+            "./about.html",
+            "./menu.html",
+
+          ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
